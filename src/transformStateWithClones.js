daebbcff99c5bbf60aeb41c16e02d1aa5a5939f4
Petro Kustov <petr.kustov@skyrora.com>
2025-05-22 16:23:24 +0300

    done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,33 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateArray = [];
+  const currentState = { ...state };
+
+  for (const actionsKey in actions) {
+    const { type, extraData, keysToRemove } = actions[actionsKey];
+
+    switch (type) {
+      case 'addProperties':
+        Object.assign(currentState, extraData);
+        stateArray.push({ ...currentState });
+        break;
+      case 'removeProperties':
+        for (const keysToRemoveKey in keysToRemove) {
+          delete currentState[keysToRemove[keysToRemoveKey]];
+        }
+        stateArray.push({ ...currentState });
+        break;
+      case 'clear':
+        for (const stateKeys in currentState) {
+          delete currentState[stateKeys];
+        }
+        stateArray.push({ ...currentState });
+        break;
+    }
+  }
+
+  return stateArray;
 }
 
 module.exports = transformStateWithClones;
